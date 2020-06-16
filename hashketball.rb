8fcc1fa32e3086786b0d7ffa1056421bf4c2a951
scruffy-node-9597 <gordonbrian16@gmail.com>
2020-06-16 03:08:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -55,22 +55,22 @@ def game_hash
   #   #   :steals =>4,
   #   #   :blocks =>11,
   #   #   :slam_dunks =>1}
-  #   ]
-  # # },
-  # # away:{
-  # #   team_name: "Charlotte Hornets",
-  #   colors: ["Turquoise, Purple"],
-  #   players: [
-  #     {:player_name => "Jeff Adrien",
-  #     :number => 4,
-  #     :shoe => 18,
-  #     :points => 10,
-  #     :rebounds => 1,
-  #     :assists => 1,
-  #     :steals => 2,
-  #     :blocks => 7,
-  #     :slam_dunks => 2},
-       
+     ]
+    },
+    away:{
+      team_name: "Charlotte Hornets",
+     colors: ["Turquoise, Purple"],
+     players: [
+      {:player_name => "Jeff Adrien",
+       :number => 4,
+       :shoe => 18,
+       :points => 10,
+       :rebounds => 1,
+       :assists => 1,
+       :steals => 2,
+       :blocks => 7,
+       :slam_dunks => 2},
+     
   #     {:player_name => "Bismack Biyombo",
   #     :number => 0,
   #     :shoe => 16,
